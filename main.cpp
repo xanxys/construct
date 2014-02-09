cbeb6fb2322eb7a204f19f71ba8f6cc7b7fa8dc7
xanxys <xanxys@gmail.com>
2014-02-09 16:45:26 +0900

    Remove lengthy comments in lieu of new README.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,3 @@
-// "Must" for NPR, comfortable rendering:
-// * SSAO (uniform lighting and still look good)
-// * FSAA (remove edge artifacts)
-//
-// Pipeline (no per-Geometry "artistic" settings).
-// everything is fixed at reasonable parameters.
-//
-// G-buffer (pos, normal, albedo) -> 
-
-// In-VR interactions:
-// Provide text input that always works. (dasher)
-// But avoid using text.
-
-// All objects are isolated from each other;
-// Object doesn't have address of other objects.
-//
-// Object-object interaction is limited to:
-// 1 geometric neighbor access
-// 2 forking itself
-// 3 sending json object via id
-//
-// 1 and 2 should be used primarily, and 3 "very" sparingly:
-// to send message from object A to distant object B,
-// it's almost always better to visualize the message as another object
-// and move it in the world.
-//
-// This way, everything is controllable from inside, by default.
-// In contrast, if 3 (or direct method call) is used mainly,
-// it can't be accessed safely from the inside because of
-// fear of infinite loop and such (see smalltalk).
-
 #include <string>
 
 #include "core.h"
